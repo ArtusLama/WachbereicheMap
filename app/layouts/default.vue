f738f12b1d00b921d625794b07c30c93a0c054a7
Artus <artuslama@gmail.com>
2025-11-10 17:08:09 +0100

    🎨 minor design change

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
     <UiSidebarProvider>
         <div class="min-h-screen flex flex-col w-full">
             <NavigationBar />
-            <main class="flex-1 px-16 py-16 mt-16 h-[calc(100vh-4rem)]">
+            <main class="flex-1 px-16 py-16 mt-8">
                 <slot />
             </main>
         </div>
